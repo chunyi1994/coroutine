3bdd3ee403cf7b771213e10c1bbfa1c8edea9cd6
sunfishgao <704613309@qq.com>
2017-03-11 11:20:33 -0600

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "coroutine.h"
+using namespace std;
+using namespace coroutine;
+void func(Coroutine* s) {
+    //int id = s->running_coroutine();
+    for (int i = 0; i <5; ++i) {
+        cout<<"正在运行的id"<<s->id()<<":"<<i<<endl;
+        s->yield();
+    }
+}
+
+void func2(Coroutine* s) {
+    //int id = s->running_coroutine();
+    for (int i = 0; i <15; ++i) {
+        cout<<"正在运行的id"<<s->id()<<":"<<i<<endl;
+        s->yield();
+    }
+}
+int main()
+{
+    Schedule s;
+    s.new_coroutine(func);
+    s.new_coroutine(func2);
+
+    while (!s.empty()) {
+        s.resume_all();
+    }
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
